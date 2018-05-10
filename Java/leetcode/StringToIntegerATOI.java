80a56a57fd91cac27da6de02556b3f0292adedd4
Rakesh Chouhan <rakesh.chouhan1@gmail.com>
2018-05-10 03:31:24 -0400

    Added comments to StringToIntegerATOI.java

--- Java/leetcode/StringToIntegerATOI.java
+++ Java/leetcode/StringToIntegerATOI.java
@@ -26,23 +26,26 @@
 * */
 public class StringToIntegerATOI {
     public int myAtoi(String str) {
-        int i = 0;
-        int result;
-        StringBuffer sb = new StringBuffer();
-        char[] charArray = str.toCharArray();
 
-        if(charArray.length == 0) {
+        str = str.trim();
+        if(str == null || str == ""){
             return 0;
         }
 
+        int i = 0;
+        StringBuffer sb = new StringBuffer();
+        char[] charArray = str.toCharArray();
+
         boolean isNegativeNumber = hasNegativeSign(charArray[0]);
-        if(str == null){
-            return 0;
-        } else if(!isNumber(charArray[0]) && !isNegativeNumber){
+        if(!isNumber(charArray[0]) && !hasSign(charArray[0])){
             return 0;
         } else {
             if(isNegativeNumber){
                 i = 1;
+            } else if(hasSign(charArray[0])){
+                i = 1;
+            } else {
+
             }
             while(i < charArray.length && isNumber(charArray[i])){
                 sb.append(charArray[i]);
@@ -61,13 +64,14 @@ public class StringToIntegerATOI {
                 // This converts a character '4' to integer 4.
                 ret += (int)(sbArray[j] - '0');
 
-                // If the index is not the last character, then multiply the integer with 10. So 4 * 10 = 40
+                // If the index is not the last character, then multiply the integer with 10 from left to right. So 4 * 10 = 40
+                // For 543 - 1st iteration ==> 5 * 10 ==> 50 + 4 = 54 ==> 54 * 10 ==> 540 + 3 ==> 543 (since 3 was not the last last character, 3 was not * by 10)
                 if (j != sb.length()-1){
                     ret *= 10;
                 }
                 // We need to return only in the int max min limit.
                 if (ret > Integer.MAX_VALUE) {
-                    return isNegativeNumber ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                    return isNegativeNumber ? Integer.MAX_VALUE * -1 : Integer.MAX_VALUE;
                 }
             }
             return isNegativeNumber ? ret.intValue() * -1 : ret.intValue() * 1;
@@ -84,6 +88,16 @@ public class StringToIntegerATOI {
         }
     }
 
+    public boolean hasSign(char sign){
+        if(sign == '+'){
+            return true;
+        } else if(sign == '-') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public boolean isNumber(char letter){
         if(Character.isDigit(letter)){
             return true;
@@ -93,6 +107,40 @@ public class StringToIntegerATOI {
 
     public static void main(String[] args){
         StringToIntegerATOI stringToIntegerATOI = new StringToIntegerATOI();
-        System.out.println(stringToIntegerATOI.myAtoi("-9234824682346"));
+        System.out.println(stringToIntegerATOI.myAtoi("+1"));
     }
+
+    /*
+    *
+    * 36 ms runtime
+    *
+    * public int myAtoi(String str) {
+        int i = 0, sign = 1, r = 0;
+        int n = str.length();
+        if (n == 0) return 0;
+
+        while (i < n && str.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (str.charAt(i) == '+' || str.charAt(i) == '-') {
+            sign = str.charAt(i) == '+' ? 1 : -1;
+            i++;
+        }
+
+        while (i < n) {
+            int num = str.charAt(i) - '0';
+            if (num < 0 || num > 9) break;
+
+            if (r > Integer.MAX_VALUE / 10 || Integer.MAX_VALUE / 10 == r && Integer.MAX_VALUE % 10 < num) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            r = 10 * r + num;
+            i++;
+
+        }
+
+        return sign * r;
+    }*/
 }
